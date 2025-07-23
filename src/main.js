9809569c1f69f7ce407e974da1964efe6863aae8
Stephany <stephanyenne@gmail.com>
2025-07-22 21:35:46 -0300

    Plans.scss foi criado

--- src/main.js
+++ src/main.js
@@ -7,9 +7,19 @@ document.addEventListener('DOMContentLoaded', function () {
       const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`);
       escondeTodasAbas();
       aba.classList.add('shows__list--is-active');
+      removeBotaoAtivo();
+      botao.target.classList.add('shows__tabs__button--is-active');
+
     });
   }
 });
+
+function removeBotaoAtivo() {
+  const buttons = document.querySelectorAll('[data-tab-button]');
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove('shows__tab__button--is-active');
+  }
+}
 
 function escondeTodasAbas() {
   const tabsContainer = document.querySelectorAll('[data-tab-id]');
